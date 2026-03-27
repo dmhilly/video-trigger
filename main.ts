d6f00d8d07e2bbf973b1be1a8215895d7dd0c49f
purplenicole730 <jihyun.nicole.jung@gmail.com>
2026-03-27 14:59:40 -0400

    add the app

---
+++ main.ts
@@ -0,0 +1,35 @@
+import * as VIAM from "@viamrobotics/sdk";
+import Cookies from "js-cookie";
+
+let apiKeyId = "";
+let apiKeySecret = "";
+let host = "";
+let machineId = "";
+
+async function main() {
+  const opts: VIAM.ViamClientOptions = {
+    serviceHost: "https://app.viam.com",
+    credentials: {
+      type: "api-key",
+      payload: apiKeySecret,
+      authEntity: apiKeyId,
+    },
+  };
+
+  const client = await VIAM.createViamClient(opts);
+  const machine = await client.appClient.getRobot(machineId);
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  // Extract the machine identifier from the URL
+  let machineCookieKey = window.location.pathname.split("/")[2];
+  ({
+    apiKey: { id: apiKeyId, key: apiKeySecret },
+    machineId: machineId,
+    hostname: host,
+  } = JSON.parse(Cookies.get(machineCookieKey)!));
+
+  main().catch((error) => {
+    console.error("encountered an error:", error);
+  });
+});
